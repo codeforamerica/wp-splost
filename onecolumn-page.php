087bd1440a245c7ac24e4551a7cde823b01efa14
jllord <jessica@codeforamerica.org>
2012-10-27 09:08:10 -0400

    make one-column not a template

--- onecolumn-page.php
+++ onecolumn-page.php
@@ -1,11 +1,6 @@
 <?php
 /**
  *
- * A custom page template without sidebar.
- *
- * The "Template Name:" bit above allows this to be selectable
- * from a dropdown menu on the edit page screen.
- *
  * @package WordPress
  * @subpackage Starkers
  * @since Starkers 3.0
